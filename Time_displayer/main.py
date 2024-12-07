9bef8c9eb7bc3c30e050c511c84d1061e0fb582b
Nursultan0099 <arsen.agent47@gmail.com>
2024-12-07 19:19:13 +0600

    Nurs: add fail module_adilet.py; changed fail main.py

---
+++ Time_displayer/main.py
@@ -0,0 +1,25 @@
+from modle_nurs import students_grades,students_list
+
+def show_students(students):
+    for i in students:
+        print(i)
+
+show_students(students_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Time_displayer/modle_nurs.py
@@ -0,0 +1,17 @@
+students_list=[
+    "esen",
+    "altynai",
+    "azamat",
+    "kylych",
+    "arsen"
+]
+
+max_score = 30
+
+students_grades = (
+    (15,max_score),
+    (12,max_score),
+    (30,max_score),
+    (1,max_score),
+    (99,max_score)
+)
